2c4f6f6ee4b478936646b4d22e2d5bb31f2936f8
Md. Raisul Islam <incognito.rochi@gmail.com>
2014-07-02 20:45:00 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+####################################################################
+# Fast and Easy Web Server Installation (Optimized for WordPress)
+# This script help you to install and configure a Linux server with:
+#	- TRAC
+#	- SVN
+#	- Iptables (More secure)
+#	- SSH (Change ports by default and securitize)
+#	- Apache, PHP
+#	- Django (Web Framework of awesome Python)
+#	- MySQL
+#	- Mail Server
+#	- Mail Server with Interface
+# Author: Coto Augosto C.
+# Edited: Raisul Islam.
+# Help: Raisul Islam <raisul.me@gmail.com>
+# URL: http://rochi.netii.net
+# Created: July 2, 2014 
+####################################################################
+
+
+readonly base_file=`readlink -f "$0"`
+readonly base_path=`dirname $base_file`
+if [ ! -f $base_path/config ]; then
+	echo "Error: You must to create a '$base_path/config' file before, you can create it from '$base_path/config.sample' "
+	exit
+fi
+
+. "$base_path/config"
+. "$base_path/functions/global.sh"
+. "$base_path/lib/mailserver.sh"
+. "$base_path/lib/mailserver_com.sh"
+. "$base_path/lib/cron-backup.sh"
+. "$base_path/lib/hook-svn/hooks_svn.sh"
+. "$base_path/lib/dns.sh"
+. "$base_path/lib/core.sh"
+
+if [[ $DistroBasedOn = "debian" ]]; then
+	. "$base_path/functions/debian.sh"
+elif [[ $DistroBasedOn = "redhat" ]]; then
+	. "$base_path/functions/redhat.sh"
+else
+	echo "Impossible to run this script in your computer, because it's not a recognizable distribution!!!"
+fi
+
+printMenu
